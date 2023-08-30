39323d3136b729f0b3b6ff0de602ae11111e62c5
SarahAlthobaiti <120081509+SarahAlthobaiti@users.noreply.github.com>
2023-08-30 11:24:45 +0000

    co3

--- lap1.java
+++ lap1.java
@@ -1,8 +1,9 @@
 // Name: Sarah Mohammed Althobiti , Student ID: 444004881.
+import java.util.Scanner;
 public class lap1 {
 
-    puplic
-    static void insertElement(int[] A, int count, int key) {
+    
+    public static void insertElement(int[] A, int count, int key) {
 
         if (count == A.length) {
             System.out.println("The array is full! ");
@@ -10,10 +11,11 @@ public class lap1 {
             A[count] = key;
             count++;
         }
+        print(A, count);
 
     }
 
-    public static int Sarah(int[] A, int count, int key) {
+    public static int Search(int[] A, int count, int key) {
 
         for (int i = 0; i < count; i++) {
             if (A[i] == key) {
@@ -28,14 +30,15 @@ public class lap1 {
         if (count == 0) {
             System.out.println("The Array is empty");
         } else {
-            i = Search(A, count, key);
+          int  i = Search(A, count, key);
             if (i == -1) {
                 System.out.println("The element not found");
             } else {
                 for (int s = i; s < count - 1; s++) {
-                    A[j] = A[j + 1];
+                    A[s] = A[s + 1];
                 }
                 count--;
+                print(A, count);
             }
         }
     }
@@ -43,13 +46,15 @@ public class lap1 {
     public static void sort(int[] A, int count){
         int temp;
         for(int i = 0; i < count; i++){
-            for(int j = i+1 ; j < count ; j++)
+            for(int j = i+1 ; j < count ; j++){
              if(A[i] > A[j]){
-                temp =  A[i]
+                temp =  A[i];
                 A[i] =  A[j];
                 A[j] = temp;
              }
+            }
         }
+        print(A, count);
     }
 
     public static void removeDuplicat(int[] A, int count){
@@ -61,6 +66,72 @@ public class lap1 {
                 }
             }
         }
+        print(A, count);
+    }
+    public static void print(int[] A, int count){
+        for(int i = 0; i < count; i++){
+            System.out.print(A[i]+" ");
+        }
+    }
+
+    public static void main(String[] args){
+
+        int[] myArray = new int[20];
+        myArray[0] = 1;
+        myArray[1] = 3;
+        myArray[2] = 2;
+        myArray[3] = 5;
+        myArray[4] = 5;
+        int count = 4;
+        boolean flag = true;
+
+        while(flag){
+
+            Scanner scnr = new Scanner(System.in);
+
+            System.out.println("\nPlease choose the number of the operation you want to perform it on an Array:\n");
+            System.out.println("1. Insert an element in the array");
+            System.out.println("2. Search for an element and return its index");
+            System.out.println("3. Delete an element from the array");
+            System.out.println("4. Sort the array");
+            System.out.println("5. Remove duplicate elements from the array");
+            System.out.println("6. Exit");
+            int opNum = scnr.nextInt();
+            if(opNum == 1){
+                System.out.println(" Enter the element");
+                int key = scnr.nextInt();
+                insertElement(myArray, count, key);
+
+            }else if(opNum == 2){
+              System.out.println(" Enter the element");
+              int key = scnr.nextInt();
+              int i = Search(myArray, count, key);
+              if(i == -1){
+                System.out.println("Not found");
+              }else{
+                System.out.println("the index of the element = "+i);
+              }
+                
+            }else if(opNum == 3){
+                System.out.println(" Enter the element");
+                int key = scnr.nextInt();
+                delete(myArray, count, key);
+            }else if(opNum == 4){
+                sort(myArray, count);
+            }else if(opNum == 5){
+                removeDuplicat(myArray, count);
+            }else if(opNum == 6){
+                flag = false;
+            }
+
+
+        }
+        System.out.println("Thank you");
+        
+
+
+        
+         
     }
 
 }
